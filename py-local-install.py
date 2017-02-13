31731b06ce520ab22a2b9d9529725dd5bb672a1e
Jack Min <jmin@cchs.com>
2017-02-13 17:04:50 -0500

    supports different folder structures

--- py-local-install.py
+++ py-local-install.py
@@ -18,11 +18,15 @@ installedRepoNameArr = []
 installationQueue = deque([])
 
 class repoClass:
-    def __init__(self, thisRepoName, thisGitStatus, thisDependencies, thisDependedBy):
+    def __init__(self, thisRepoName, thisGitStatus, thisDependencies, thisDependedBy, thisPath):
         self.repoName = thisRepoName
         self.gitStatus = thisGitStatus
         self.dependencies = thisDependencies
         self.dependedBy = thisDependedBy
+        self.path = thisPath
+
+rootPath = os.getcwd()
+
 
 # Get all cdsp repos
 subp0 = subprocess.Popen(['curl', '-s', "https://api.github.com/orgs/monsterstack/repos"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -31,6 +35,8 @@ op = json.loads(curlstdout)
 for repo in op:
     cdspRepoNameArr.append(repo["name"])
 
+
+# Starts
 script = ""
 changedReopsStr = ""
 if len(sys.argv) == 2:
@@ -43,13 +49,19 @@ else:
 changedReops = changedReopsStr.split(",")
 
 
-#Get local git repos and its dependencies
-localFolders = [dname for dname in os.listdir(".") if (os.path.isdir(dname))]
+# Get repo absolute path
+subp1 = subprocess.Popen('find `pwd` -type d -name \".git\"', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+findstdout , findstderr = subp1.communicate()
+localReposPath = findstdout.split(".git\n")[:-1]
+
+
+# Get local git repos and its dependencies and absolute path
 localRepos = {}
 localReposNameArr = []
 basicRepos = []
-for folder in localFolders:
-    os.chdir(folder)
+for thisPath in localReposPath:
+    folder = thisPath.split("/")[-2]
+    os.chdir(thisPath)
     if (os.path.isdir(".git")):
         thisDependencies = []
         if(os.path.exists("package.json")):
@@ -60,14 +72,14 @@ for folder in localFolders:
         for dependency in thisPackageJson["dependencies"]:
             if dependency in cdspRepoNameArr:
                 thisDependencies.append(dependency)
-        localRepos[folder] = repoClass(folder, None, thisDependencies, [])
+        localRepos[folder] = repoClass(folder, None, thisDependencies, [], thisPath)
         if (len(thisDependencies)==0):
             basicRepos.append(folder)
         localReposNameArr.append(folder)
-    os.chdir("..")
+    os.chdir(rootPath)
 
 
-#Find affected repos
+# Find affected repos
 affectedRepoNameArr = []
 queue = deque(changedReops)
 while(len(queue)!= 0):
@@ -92,7 +104,7 @@ affectedRepoNameArr.sort()
 # print affectedRepoNameArr
 
 
-#install
+# Install
 while (affectedRepoNameArr != installedRepoNameArr):
     for theRepoName, theRepoInfo in localRepos.iteritems():
         #get affected dependencies
@@ -106,7 +118,7 @@ while (affectedRepoNameArr != installedRepoNameArr):
 
     while (len(installationQueue)!=0):
         temp = installationQueue.popleft()
-        os.chdir(temp)
+        os.chdir(localRepos.get(temp).path)
         print temp
 
         if (os.path.exists('node_modules')==False):
@@ -125,7 +137,7 @@ while (affectedRepoNameArr != installedRepoNameArr):
 
         if(temp not in installedRepoNameArr):
             installedRepoNameArr.append(temp)
-        os.chdir("..")
+        os.chdir(rootPath)
     installedRepoNameArr.sort()
     # print installedRepoNameArr
 
