6e3753b3149ca87bfae69b13384f7b22da9f6ea5
Jack Min <jmin@cchs.com>
2017-03-02 10:56:00 -0500

    add cleanInstall and initInstall bash scripts

---
+++ pull-and-install-from-development_fresh.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+BRANCH=$1
+
+if [ $# -eq 0 ]
+  then
+    BRANCH="development"
+fi
+
+echo $BRANCH
+
+# CDSP_REPOS=`curl -s https://api.github.com/orgs/monsterstack/repos | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| puts "#{repo["name"]}" }'`
+# echo -e "CDSP_REPOS: \n$CDSP_REPOS"
+
+REPOS=`find \`pwd\` -name ".git"|rev|cut -d "/" -f 2-|rev`
+# echo $SERVICE_REPOS
+
+
+REPO_ROOT=`pwd`
+
+
+for REPO in ${REPOS}; do
+    cd ${REPO}
+    # `rm -rf node_modules`
+    pullCommand=`git pull origin $BRANCH`
+    echo "$pullCommand"
+    findPackageJsonCommand=`find . -maxdepth 1 -iname "package.json"`
+    if [ "$findPackageJsonCommand" != "" ]
+        then
+            `rm -rf node_modules`
+            `npm install > /dev/null`
+    fi
+    cd ${REPO_ROOT}
+done
+cd ${REPO_ROOT}
