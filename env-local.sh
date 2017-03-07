99dad4ced7673ccd24f897848821a9ea9e2654e6
zacharyrote <zachary.rote@gmail.com>
2017-03-07 13:48:58 -0500

    local env

--- env-local.sh
+++ env-local.sh
@@ -15,14 +15,17 @@ export RETHINK_HOST_IP=$IP
 export REDIS_HOST_IP=$IP
 export MONGO_HOST_IP=$IP
 export ETCD_HOST_IP=$IP
+export DISCOVERY_HOST_IP=$IP
 
 # Remove current line with hostname at the end of line ($ means end of line)
 sed '/etcd.digitalfunk.io/d' /etc/hosts 
 sed '/etcd.digitalfunk.io/d' /etc/hosts
 sed '/mongo.digitalfunk.io/d' /etc/hosts
 sed '/redis.digitalfunk.io/d' /etc/hosts
+sed '/discovery.digitalfunk.io/d' /etc/hosts
 
 echo "$IP rethink.digitalfunk.io" >>/etc/hosts
 echo "$IP etcd.digitalfunk.io" >>/etc/hosts
 echo "$IP mongo.digitalfunk.io" >>/etc/hosts
 echo "$IP redis.digitalfunk.io" >>/etc/hosts
+echo "$IP discovery.digitalfunk.io" >>/etc/hosts
